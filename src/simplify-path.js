ada5793b0de70c9b57b91bdc817ba9b15a3aebe2
Hongbusi <q951686662@qq.com>
2022-01-06 14:33:36 +0800

    feat: simplify-path

---
+++ src/simplify-path.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+
+var simplifyPath = function(path) {
+  const names = path.split('/');
+  const stack = [];
+  for (const name of names) {
+    if (name === '..') {
+      if (stack.length) {
+        stack.pop();
+      }
+    } else if (name.length && name !== '.') {
+      stack.push(name);
+    }
+  }
+
+  return '/' + stack.join('/');
+};
