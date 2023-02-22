dbabb615d7eb6257c130a88708c7aedafe8bd2dc
Hongbusi <coderhbs@gmail.com>
2023-02-22 23:37:32 +0800

    chore: update

--- hzoj/184.路飞吃桃.js
+++ hzoj/184.路飞吃桃.js
@@ -1,5 +1,9 @@
 // https://oj.haizeix.com/problem/184
 
 function peachCount(day) {
-
+  if (day === 1) return 1
+  return (peachCount(day - 1) + 1) * 2
 }
+
+console.log(peachCount(2))
+console.log(peachCount(3))
--- hzoj/186.弹簧板.js
+++ hzoj/186.弹簧板.js
@@ -1,1 +1,18 @@
 // https://oj.haizeix.com/problem/186
+
+function f(i, arr, n) {
+  if (i >= n) return 0
+  return f(i + arr[i], arr, n) + 1
+}
+
+function main(n) {
+  const arr = []
+  for (let i = 0; i < n; i++) {
+    arr.push(Math.floor(Math.random() * 10))
+  }
+
+  console.log(arr)
+  console.log(f(0, arr, n))
+}
+
+main(5)
